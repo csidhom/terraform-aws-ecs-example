3f8040f3c6a354353906f8566995538d37b6385b
Chaker Sidhom <csidhom@readytech.com>
2020-10-30 13:35:37 +0100

    Attach the ALB to the ASG

--- alb.tf
+++ alb.tf
@@ -1,7 +1,35 @@
+/*
+* Define the ALB Security Group
+* Allow inbound traffic on port 80 from anywhere
+* Allow outbound traffic on all ports
+*/
+resource "aws_security_group" "alb_sg" {
+  name   = "${var.prefix_name}-alb-sg"
+  vpc_id = module.vpc.vpc_id
+}
+
+resource "aws_security_group_rule" "inbound_access" {
+  type              = "ingress"
+  from_port         = 80
+  to_port           = 80
+  protocol          = "tcp"
+  cidr_blocks       = ["0.0.0.0/0"]
+  security_group_id = aws_security_group.alb_sg.id
+}
+
+resource "aws_security_group_rule" "outbound_internet_access" {
+  type              = "egress"
+  from_port         = 0
+  to_port           = 0
+  protocol          = "-1"
+  cidr_blocks       = ["0.0.0.0/0"]
+  security_group_id = aws_security_group.alb_sg.id
+}
+
 /* Create an Application Load Balancer in the public subnet */
 resource "aws_alb" "ecs-load-balancer" {
   name            = "ecs-load-balancer"
-  security_groups = ["${aws_security_group.alb_public_sg.id}"]
+  security_groups = ["${aws_security_group.alb_sg.id}"]
   subnets         = module.vpc.public_subnets
 
   tags = {
@@ -43,26 +71,3 @@ resource "aws_alb_listener" "alb-listener" {
   }
 }
 
-/*
-* Define the ALB Security Group
-* Allow inbound traffic on port 80 from anywhere
-* Allow outbound traffic on all ports
-*/
-resource "aws_security_group" "alb_public_sg" {
-  name   = "${var.prefix_name}-alb-sg"
-  vpc_id = module.vpc.vpc_id
-
-  ingress {
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  egress {
-    from_port   = 0
-    to_port     = 65535
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-}
--- autoscaling.tf
+++ autoscaling.tf
@@ -14,37 +14,27 @@ data "aws_ami" "amazon_linux_ecs" {
   }
 }
 
-/* Create ECS security Group */
-resource "aws_security_group" "ecs_sg" {
+/* Create ECS security Group for outbound internet access  */
+resource "aws_security_group" "instance_sg" {
+  name   = "${var.prefix_name}-instance-sg"
   vpc_id = module.vpc.vpc_id
-
-  ingress {
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  ingress {
-    from_port   = 443
-    to_port     = 443
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
 
   egress {
     from_port   = 0
-    to_port     = 65535
-    protocol    = "tcp"
+    to_port     = 0
+    protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
 }
 
-/* Create launch configuration and ASG to scale EC2 instances in the private subnet */
+/* 
+ * Create launch configuration and ASG to scale EC2 instances in the private subnet 
+ * The parameter `target_group_arns` is used to attach the ASG to an ALB
+*/
 resource "aws_launch_configuration" "ecs_launch_config" {
   image_id             = data.aws_ami.amazon_linux_ecs.id
   iam_instance_profile = aws_iam_instance_profile.ecs.id
-  security_groups      = [aws_security_group.ecs_sg.id]
+  security_groups      = [aws_security_group.instance_sg.id]
   user_data            = "#!/bin/bash\necho ECS_CLUSTER=my-cluster >> /etc/ecs/ecs.config"
   instance_type        = var.instance_type
 }
@@ -53,10 +43,11 @@ resource "aws_autoscaling_group" "ecs_asg" {
   name                 = "${var.prefix_name}-asg"
   vpc_zone_identifier  = module.vpc.private_subnets
   launch_configuration = aws_launch_configuration.ecs_launch_config.name
+  target_group_arns    = [aws_alb_target_group.ecs-target-group.arn]
 
   desired_capacity          = 2
   min_size                  = 2
   max_size                  = 2
-  health_check_grace_period = 300
-  health_check_type         = "EC2"
+  health_check_grace_period = 60
+  health_check_type         = "ELB"
 }
